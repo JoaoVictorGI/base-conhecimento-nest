39f7adecea80c31471ce109fcf89f9af568b75f4
João Victor <joaovictor.gizidoro@gmail.com>
2025-02-19 20:49:04 -0300

    feat: update database configuration to use environment variables and add SSL support

--- mikro-orm.config.ts
+++ mikro-orm.config.ts
@@ -2,12 +2,17 @@ import { Options } from '@mikro-orm/core';
 import { PostgreSqlDriver } from '@mikro-orm/postgresql';
 
 const mikroOrmConfig: Options = {
-  dbName: 'nestjs',
-  user: 'admin',
-  password: 'admin',
-  host: 'localhost',
-  port: 5432,
+  dbName: process.env.POSTGRES_DB,
+  user: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  host: process.env.POSTGRES_HOST,
+  port: Number(process.env.POSTGRES_PORT),
   driver: PostgreSqlDriver,
+  driverOptions: {
+    connection: {
+      ssl: { rejectUnauthorized: false }
+    },
+  },
   entities: ['./dist/**/*.entity.js'],
   entitiesTs: ['./src/**/*.entity.ts'],
 };
