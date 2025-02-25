70e7188b8eb57b35c2848af3ac3637c7962e78bc
João Victor <joaovictor.gizidoro@gmail.com>
2025-02-25 10:31:01 -0300

    fix: set authentication cookie domain to .onrender.com for correct deployment usage

--- src/authentication/authentication.service.ts
+++ src/authentication/authentication.service.ts
@@ -81,7 +81,7 @@ export class AuthenticationService {
     const payload: TokenPayload = { userId };
     const token = this.jwtService.sign(payload);
 
-    return `Authentication=${token}; HttpOnly; Path=/; Max-Age=${this.configService.get('JWT_EXPIRATION_TIME')}; SameSite=None; Secure`;
+    return `Authentication=${token}; HttpOnly; Domain=.onrender.com; Path=/; Max-Age=${this.configService.get('JWT_EXPIRATION_TIME')}; SameSite=None; Secure`;
   }
 
   public getCookieForLogOut() {
